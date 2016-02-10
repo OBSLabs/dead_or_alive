e2d59aa20c6bf4ef51568364f9234113fef5dedd
Alexander Petrov <alexonrails@gmail.com>
2016-02-11 02:13:23 +0300

    add missing yield

--- app/services/sidekiq_middleware.rb
+++ app/services/sidekiq_middleware.rb
@@ -1,6 +1,7 @@
 module DeadOrAlive
   class SidekiqMiddleware
     def call(worker_instance, item, queue)
+      yield
       DeadOrAlive::ControllerRepo.new.worker!(worker_instance.class)
     end
   end
